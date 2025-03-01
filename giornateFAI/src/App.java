aa56f052c169e5f2bad919358ea2882e5845bf05
Federico <federicoschettino32@gmail.com>
2025-03-01 16:46:16 +0100

    Inserito DriverSQL e fatto prove di connessione riuscite al DB locale

--- giornateFAI/src/App.java
+++ giornateFAI/src/App.java
@@ -1,5 +1,32 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+         Connection conn = ConnesioneSQL.getConnection();
+        if (conn != null) {
+            try {
+
+                //inserisce un nuovo fruitore
+                String inserimentoProva = "INSERT INTO `dbingesw`.`fruitori` (`Nickname`, `Password`) VALUES ('paola3prova', 'qidubhiodb')";
+                conn.createStatement().executeUpdate(inserimentoProva);
+                System.out.println("Inserimento riuscito!");
+
+                //legge la tabella fruitori
+                String selectProva = "SELECT * FROM fruitori"; 
+                ResultSet rs = conn.createStatement().executeQuery(selectProva);
+                while (rs.next()) {
+                    System.out.println("Nickname: " + rs.getString("Nickname") + " Password: " + rs.getString("Password"));
+                }
+
+                conn.close(); // Chiude la connessione
+                System.out.println("Connessione chiusa.");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
---
+++ giornateFAI/src/ConnesioneSQL.java
@@ -0,0 +1,31 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnesioneSQL {
+    // Dati per la connessione
+    private static final String URL = "jdbc:mysql://localhost:3306/dbingesw";
+    private static final String USER = "root"; 
+    private static final String PASSWORD = "root"; 
+
+    public static Connection getConnection() {
+        try {
+            // Carica il driver MySQL
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Crea la connessione
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connessione riuscita!");
+            return conn;
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver non trovato!");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.out.println("Errore nella connessione!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
