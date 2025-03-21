019494eaa16a430e4fab83c9b05dcf1191636498
MadMath02 <diegopiccoli2002@gmail.com>
2025-03-21 16:29:13 +0100

    Compilazione

--- compile.bat
+++ compile.bat
@@ -1,1 +1,3 @@
-javac -sourcepath src -classpath "Librerie Esterne\mysql-connector-j-9.2.0.jar" -classpath "Librerie Esterne\UniBSFpLib.jar" -d giornateFAI/bin giornateFAI/src/giornateFAI/*
+setlocal enabledelayedexpansion
+for /f "delims=" %%a in ('dir /b /s /a-d giornateFAI\src') do set "files=!files! %%a"
+javac -sourcepath giornateFAI/src -classpath "Librerie Esterne\mysql-connector-j-9.2.0.jar";"Librerie Esterne\UniBSFpLib.jar";"Librerie Esterne\jBCrypt.jar" -d giornateFAI/bin %files%
